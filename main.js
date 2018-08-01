6fab2e2e40111c8d334a5b04c3384c2dd5f4b6b6
Tammy Lee <tttlee@ucdavis.edu>
2018-07-31 22:15:22 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+var check1 = document.getElementById('c1');
+var check2 = document.getElementById('c2');
+var check3 = document.getElementById('c3');
+var check4 = document.getElementById('c4');
+
+
+function fillColor1(){
+            check1.setAttribute("style","background-color: pink");
+}
+
+function fillColor2(){
+            check2.setAttribute("style","background-color: pink");
+}
+
+function fillColor3(){
+            check3.setAttribute("style","background-color: pink");
+}
+
+function fillColor4(){
+            check4.setAttribute("style","background-color: pink");
+}
+function remindMe(){
+    alert("Reminder sent!");
+}
